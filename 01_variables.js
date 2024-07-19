16db4775b3b6c34be627d95725997bcfa43a19da
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-19 20:38:05 +0530

    more lectures attend

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 533417
+let accountemail = "abhaytewatia07@gmail.com"
+var accountpassword = "45652"
+accountcity = "gurugram"
+let accountstate;
+// account id = 5// const change not allowed
+accountemail = "abhayt@gmail.com"
+accountpassword = "45"
+accountcity = "noida" 
+console.log(accountId);
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountemail, accountpassword, accountcity, accountstate]);
---
+++ 02_datatypes.js
@@ -0,0 +1,38 @@
+ "use strict"; //treat all JS code as newer version
+// alert(3+3) we are using nodejs, not browser
+console.log(3+3);
+
+console.log("abhay");
+
+let name ="abhay"
+let age  = 21
+let isloggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => null
+// undefined => undefined
+
+
+// object => {}
+    // symbol => unique
+
+
+
+    // function => function(){}
+    // array => []
+        // map => new Map()
+        // set => new Set()
+        // weakmap => new WeakMap()
+        // weakset => new WeakSet()
+
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof isloggedIn)
+ console.log(typeof state)
+ console.log(typeof null)
+ console.log(typeof undefined)
