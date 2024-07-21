525a8f43268c8df34e6653dcbad1153de79e6f66
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-21 11:14:42 +0530

    summary

---
+++ 06_summaryofdatatypes.js
@@ -0,0 +1,42 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100 
+const scoreValue   = 100.3
+
+const isScoreValid = true
+
+
+const outsideTemp = null
+
+
+let userEmail;
+
+// undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// both id are not equal after we can check
+
+
+// we cannot defined datatypes in javascript 
+
+// Reference (non primitive)
+
+//  Array, Objects, Functions
+
+
+const heros = ["saktiman", "ironman", "thor"]
+
+let myObj = {
+    name :"abhay",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("hello")
+}
+
+console.log(typeof heros)
