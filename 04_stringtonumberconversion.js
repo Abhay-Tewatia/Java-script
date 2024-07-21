be08e9504fd100b42c0bea978b125329dff6a47e
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-21 09:31:39 +0530

    operations

---
+++ 04_stringtonumberconversion.js
@@ -0,0 +1,37 @@
+// operations 
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Abhay"
+// let str2 = " Tewatia"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log( (3 +4) * 5 % 3);
+
+// console.log(true);
+// console.log(+true);
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 =2+2
+
+
+let gameCounter = 1000
+++gameCounter;
+console.log(gameCounter);
+
