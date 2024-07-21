0f751c9f587fa9b2a214b669d285209afb6488e6
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-21 09:11:06 +0530

    conversion of numbers

--- hello.js
+++
@@ -1,6 +0,0 @@
- console.log("Abhay Tewatia")
-
-//  node -v for checking environment succesfully installed or not
-// if Number  is in output  it is installed
-//  now for run a code 
-// node and filename.js
