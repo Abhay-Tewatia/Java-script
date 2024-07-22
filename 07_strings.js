7ca6ad3b523afcb4eb65e38a77faf3c4d4974a86
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-22 12:18:55 +0530

    string in js

---
+++ 07_strings.js
@@ -0,0 +1,36 @@
+// const name = "Abhay "
+// const repocount = 50
+
+// console.log(name + repocount + " Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+// const gameName = new String('Abhay') 
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__.);
+
+// console.log(gameName.length);
+// console.log(gameName.toUppercase());
+
+// console.log(gameName.charAt(2));
+// // console.log(gameName.indexOf('b'));
+
+// const newstring = gameName.substring(0, 3)
+// console.log(newstring);
+
+
+// we can use slice
+// console.log(gameName.slice(-4, 3));
+
+// const newStringOne = "   Abhay  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://abhay.com/abhya30%Tewatia"
+// console.log(url.replace('30%', '-'))
+
+// const gameName = ("this is abhay tewatia")
+// console.log(gameName.split('-'));
+
+
