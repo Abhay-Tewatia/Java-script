5209a17bc8f589a2a7e55d0c0dc7f2009ad1e8bf
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-26 22:52:45 +0530

    arrays in js

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,43 @@
+// array
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArray)
+// console.log(myArray[2])
+
+const myHeroes = ["Ironman", "Batman", "Saktiman"]
+// console.log(myHeroes[1])
+
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[2])
+
+// Array methods
+// myArr.push(6)
+// myArr.push(7)
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// console.log(type"of newArr);
+
+// slice   Splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+
+console.log(myn2);
+console.log("C ", myArr);
