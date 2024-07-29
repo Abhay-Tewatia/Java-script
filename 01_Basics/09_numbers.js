ca60ec036b2c590d8d7cbe492410bc44949800ea
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-29 23:23:23 +0530

    math in js

--- 01_Basics/09_numbers.js
+++ 01_Basics/09_numbers.js
@@ -18,7 +18,22 @@ const Bikeprice = 1000000
 // ----maths----
 
 console.log(Math)
+console.log(Math.abs(-4));
+// this make the Node. positive
 
+// console.log(Math.abs(4));
 
+// console.log(Math.round((4.4)))
 // console.log(Math.floor(4.9));
 // console.log(Math.ceil(4.2));
+// console.log(Math.min(4,3,7,9,5,2));
+// console.log(Math.max(4,3,7,9,5,2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10 +1);
+// console.log((Math.random()*10) +1)
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (max - min +1) + min )));
