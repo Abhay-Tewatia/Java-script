e09892a06c7aed8fe26d94f55c6e69abbbb20f00
Abhay Tewatia <abhaytewatia342@gmail.com>
2024-07-29 22:47:25 +0530

    math in js

--- 01_Basics/09_numbers.js
+++ 01_Basics/09_numbers.js
@@ -1,2 +1,24 @@
-console.log(Math.floor(4.9));
-console.log(Math.ceil(4.2));
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+
+const price = 45.969
+// console.log(price.toFixed(2));
+// console.log(price.toPrecision(2));
+
+const Bikeprice = 1000000
+// console.log(Bikeprice.toLocaleString('en-IN'));
+
+// ----maths----
+
+console.log(Math)
+
+
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.2));
